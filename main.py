593821ade518af0df95580943d49307d7cafcae6
kuotony <39187028+kuotony@users.noreply.github.com>
2026-01-31 12:58:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,8 @@ def fetch_and_process(station_code, url):
                 # --- 資料處理邏輯 (對每一行都做一次) ---
                 current_line = line
                 
-                # (e) 捨棄 RMK
+                # (e) 捨棄 COR 與 RMK
+                current_line = current_line.replace("COR", "")
                 if "RMK" in current_line:
                     current_line = current_line.split("RMK")[0]
                 
@@ -134,3 +135,4 @@ def main_loop():
 # 開始執行
 if __name__ == "__main__":
     main_loop()
+
